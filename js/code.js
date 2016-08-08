8d1c8c60d1ff59bc6d2ec7c0e5c0cca8e23608a4
Eli Parra <eli@elzr.com>
2016-08-07 19:39:58 -0500

    sat6aug2016 days work

--- js/code.js
+++ js/code.js
@@ -1,2 +1,28 @@
-var = QHHA = {
+var QHHA = {
+	boot:function() {
+		$('.compromisos a.compromiso').click( this.pickCompromiso );
+	},
+	sheet: {
+		zap:'1KgtTvqqNeCZn4mCQEIRFYI3Wr4P2dvQvpub3toLFtoE',
+		gdl:'1KgtTvqqNeCZn4mCQEIRFYI3Wr4P2dvQvpub3toLFtoE',
+		ejes: {
+			seg:1, urb:2, eco:3, efi:4, pub:5, med:6, com:7
+		},
+		fetch:function(zapGdl, eje) {
+			var url = 'https://spreadsheets.google.com/feeds/list/ZAPGDL/EJE/public/basic?alt=json';
+			url = url.replace(/ZAPGDL/, this[zapGdl]).replace(/EJE/, this.ejes[eje]);
+			console.log(url);
+
+			$.getJSON(url, function(data){
+				console.log(data);
+			});
+		}
+	},
+	pickCompromiso:function() {
+		$(this).parents('ul.compromisos').toggleClass('picked');
+	}
 };
+
+$( document ).ready(function() {
+	QHHA.boot();
+});
